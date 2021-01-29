926154c18ed9224abcf313a194017dfd8e984d8b
Max Clarke <maxclarke@ma368.lan>
2021-01-29 12:19:20 +0000

    all exercises for step 8 completed

--- directory_step8_exercises.rb
+++ directory_step8_exercises.rb
@@ -1,60 +1,60 @@
-#TODO 6,8,10
+#TODO 8
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, enter 'submit'"
   students = []
-  name = gets.chomp
-  
+  name = gets.delete!("\n")
+
   if name == "submit"
     students
   else
-    
-  puts "Please enter the month of cohort"  
-  cohort = gets.chomp
-  
+
+  puts "Please enter the month of cohort"
+  cohort = gets.delete!("\n")
+
   if cohort.empty?
       cohort = "january"
   end
-  
-  puts "Please enter date of birth :dd/mm/yyyy"  
-  dob = gets.chomp
-  puts "Favourite hobby"  
-  hobby = gets.chomp
-  puts "Height in cm"  
-  height = gets.chomp
+
+  puts "Please enter date of birth :dd/mm/yyyy"
+  dob = gets.delete!("\n")
+  puts "Favourite hobby"
+  hobby = gets.delete!("\n")
+  puts "Height in cm"
+  height = gets.delete!("\n")
   students << {name: name, cohort: cohort, dob: dob, hobby: hobby, height: height}
   puts "Now we have #{students.count} students"
-  name = gets.chomp
-  
+  name = gets.delete!("\n")
+
   until name == "submit" do
-    
+
         puts "Please enter the month of cohort"
-        cohort = gets.chomp
+        cohort = gets.delete!("\n")
         if cohort.empty?
           cohort = "january"
         end
-        puts "Please enter date of birth :dd/mm/yyyy"  
-        dob = gets.chomp
-        puts "Favourite hobby"  
-        hobby = gets.chomp
-        puts "Height in cm"  
-        height = gets.chomp
+        puts "Please enter date of birth :dd/mm/yyyy"
+        dob = gets.delete!("\n")
+        puts "Favourite hobby"
+        hobby = gets.delete!("\n")
+        puts "Height in cm"
+        height = gets.delete!("\n")
         students << {name: name, cohort: cohort, dob: dob, hobby: hobby, height: height}
         puts "Now we have #{students.count} students"
-        name = gets.chomp
+        name = gets.delete!("\n")
   end
   end
     students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(5)
-  puts "-------------"
+  puts "The students of Villains Academy"
+  puts "-------------".center(32)
 end
 
 #Exercise 1, each_with_index made redunant by changing from .each method to utlizing while (as per exercise 4)
-#def print(students) 
+#def print(students)
   #students.each_with_index do |student, index| #Exercise 1
     #indexplusone = index + 1 #Exercise 1 cont.
     #if student[:name].length <= 12 && student[:name].chars.first == "a" #Exercise 2 + Exercise 3
@@ -63,34 +63,36 @@ end
   #end
 #end
 
-def print(students) 
+def print(students)
 counter = 0
+puts "Which cohort?"
+answer = gets.chomp
   while counter < students.length do
     index = counter + 1
-    if students[counter][:name].length <= 12 && students[counter][:name].chars.first == "a" 
-      puts "#{index}. #{students[counter][:name]} #{students[counter][:cohort]} cohort 
-      Date of Birth: #{students[counter][:dob]} 
-      Favourite Hobby: #{students[counter][:hobby]} 
+    if students[counter][:name].length <= 12 && students[counter][:name].chars.first == "a" && students[counter][:cohort] == answer
+      puts "#{index}. #{students[counter][:name]} #{students[counter][:cohort]} cohort
+      Date of Birth: #{students[counter][:dob]}
+      Favourite Hobby: #{students[counter][:hobby]}
       Height(cm): #{students[counter][:height]}"
     end
     counter = counter + 1
   end
-end 
+end
 
 def print_footer(students)
-  if students.count > 1 
+  if students.count > 1
   puts "Overall, we have #{students.count} great students".center(18)
   else
   puts "Overall, we have #{students.count} great student".center(18)
-  end 
+  end
 end
 
 students = input_students
 
-if !students.empty? 
+if !students.empty?
 print_header
 print(students)
 print_footer(students)
-else 
+else
   puts "No students"
-end 
+end
