513600af5d0d3472488a034b9598e7940854474a
Max Clarke <maxclarke@MA368.local>
2021-01-31 15:37:57 +0000

    updated ireadmyself

--- ireadmyself.rb
+++ ireadmyself.rb
@@ -1,2 +1,8 @@
-string="string=%c%s%c; printf string,34,string,34,10%c";
-printf string,34,string,34,10
+def helloworld
+  string = "Hello World!"
+  puts string
+end
+
+helloworld
+
+puts File.read(__FILE__)
